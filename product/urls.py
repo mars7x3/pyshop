c21e0dca80b1d19270633c49edd1e895e007850e
mars7x3 <m.ysakov.jcc@gmail.com>
2021-03-29 17:56:29 +0600

    comments

---
+++ product/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from product.views import *
+
+urlpatterns = [
+    path('', home_page, name='home'),
+    path('<str:slug>/', product_list, name='list'),
+    path('product/<int:product_id>/', product_detail, name='detail'),
+    path('product/create/', create_product, name='create-product'),
+    path('product/update/<int:product_id>/', update_product, name='update-product'),
+    path('product/delete/<int:product_id>/', delete_product,
+                            name='delete-product')
+
+
+]
---
+++ product/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import *
+from .models import *
+
+
+def home_page(request):
+    categories = Category.objects.all()
+    return render(request, 'product/home.html', {
+        'categories': categories})
+
+def product_list(request, slug):
+    products = Product.objects.filter(category__slug=slug)
+    return  render(request, 'product/list.html', locals())
+
+def product_detail(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    return  render(request, 'product/detail.html', {'product': product})
+
+def create_product(request):
+    if request.method == 'POST':
+        product_form = CreateProductForm(request.POST,
+                                         request.FILES)
+        if product_form.is_valid():
+            product = product_form.save()
+            return redirect(product.get_absolut_url())
+    else:
+        product_form = CreateProductForm()
+    return render(request, 'product/create_product.html',
+                   {'product_form': product_form})
+
+def update_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    product_from = UpdateProductForm(request.POST or None,
+                                     request.FILES or None,
+                                     instance=product)
+    if product_from.is_valid():
+        product_from.save()
+        return redirect(product.get_absolut_url())
+    return render(request, 'product/update_product.html',
+                   {'product_form': product_from})
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        slug = product.category.slug
+        product.delete()
+        return redirect('list', slug)
+    return render(request, 'product/delete_product.html',
+                  {'product': product})
+
